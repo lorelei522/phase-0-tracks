846d1ecd17e79aa6d0afdb00ae76f2d008f6de13
lorelei522 <lorelei522@gmail.com>
2016-08-23 17:52:25 -0400

    Release 2 process strings to integers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,13 @@
 puts "Hamster's name?"
 hamster_name= gets.chomp
 puts "Volume Level from 1-10?"
-volume_level= gets.chomp
+volume_level= gets.to_i
 puts "Fur color?"
 fur_color= gets.chomp
 puts "Good candidate for adoption(Y/N)"
 good_candidate= gets.chomp
 puts "Estimated Age"
-estimated_age= gets.chomp
+estimated_age= gets.to_i
 	if estimated_age== ""
 		estimated_age=nil
 	else
