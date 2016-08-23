4c68d7ebe49b614da0417c36ee521bede2d7db31
lorelei522 <lorelei522@gmail.com>
2016-08-23 17:36:17 -0400

    Release 1 of 4.3

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "Hamster's name?"
+hamster_name= gets.chomp
+puts "Volume Level from 1-10?"
+volume_level= gets.chomp
+puts "Fur color?"
+fur_color= gets.chomp
+puts "Good candidate for adoption(Y/N)"
+good_candidate= gets.chomp
+puts "Estimated Age"
+estimated_age= gets.chomp
+	if estimated_age== ""
+		estimated_age=nil
+	else
+		#{estimated_age}
+end		
