2445ac769d9fd4af9610722080762d702f98883c
lorelei522 <lorelei522@gmail.com>
2016-08-24 13:57:28 -0400

    Turn user information for age and year_of_birth to integers

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,9 @@
 puts "What is your name?"
 name=gets.chomp
 puts "How old are you?"
-age= gets.chomp
+age= gets.to_i
 puts "What year were you born?"
-year_of_birth= gets.chomp
+year_of_birth= gets.to_i
 puts "Would you like garlic bread? (Y/N)"
 garlic_bread= gets.chomp
 puts "Would you like to enroll in the company's health insurance? (Y/N)"
