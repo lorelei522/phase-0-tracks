025194df7dbaad0bc4d6aa201f510a5e2a6cef16
lorelei522 <lorelei522@gmail.com>
2016-08-24 13:26:21 -0400

    Release 1,initial program ask new employee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name=gets.chomp
+puts "How old are you?"
+age= gets.chomp
+puts "What year were you born?"
+year_of_birth= gets.chomp
+puts "Would you like garlic bread? (Y/N)"
+garlic_bread= gets.chomp
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+health_insurance= gets.chomp
