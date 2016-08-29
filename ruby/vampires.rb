1e64edc4f6fdb38833afe4550c166d276006d333
lorelei522 <lorelei522@gmail.com>
2016-08-29 01:26:54 -0400

    Process multiple employees by creating a while loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,22 @@
-puts "What is your name?"
-name=gets.chomp
-puts "How old are you?"
-age_answered= gets.to_i
-puts "What year were you born?"
-year_of_birth= gets.to_i
-puts "Would you like garlic bread? (Y/N)"
-garlic_bread= gets.chomp
-puts "Would you like to enroll in the company's health insurance? (Y/N)"
-health_insurance= gets.chomp
+#Release 2 create a loop that asks how many employees will be processed 
+#and have the program process them until all employees go all through
 
-age_calculated= 2016-year_of_birth
+puts "How many employees are being processed today?"
+employee_number= gets.chomp.to_i
+
+while employee_number>0
+	puts "What is your name?"
+	name=gets.chomp
+	puts "How old are you?"
+	age_answered= gets.to_i
+	puts "What year were you born?"
+	year_of_birth= gets.to_i
+	puts "Would you like garlic bread? (Y/N)"
+	garlic_bread= gets.chomp
+	puts "Would you like to enroll in the company's health insurance? (Y/N)"
+	health_insurance= gets.chomp
+
+	age_calculated= 2016-year_of_birth
 
 #If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
@@ -27,4 +34,6 @@ elsif (age_calculated != age_answered) && (garlic_bread=="n" && health_insurance
 		puts "Almost certainly a vampire"
 else
 	puts "Results inconclusive."
+end
+	employee_number=employee_number-1
 end
