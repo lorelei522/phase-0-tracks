a29c9bea55980a77914a6cfda78b8a3e039d7769
lorelei522 <lorelei522@gmail.com>
2016-08-30 22:59:31 -0400

    Add questions and set up for replies to the question for the client

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -11,3 +11,29 @@ interior_design_client = {
 	loves_green: "",
 }
 
+# designer needs to answer the following questions for the client
+
+puts "Name of client?"
+interior_design_client[:name]=gets.chomp
+
+puts "Age of client?"
+interior_design_client[:age]=gets.chomp.to_i
+
+puts "What is the client's address"
+interior_design_client[:address]=gets.chomp
+
+puts "How many children does the client have?"
+interior_design_client[:number_of_children]=gets.chomp.to_i
+
+puts "What is their decor theme?"
+interior_design_client[:decor_theme]=gets.chomp
+
+puts "How many rooms does the client have?"
+interior_design_client[:amount_of_rooms]=gets.chomp.to_i
+
+puts "Do they love leopard(true or false)?"
+interior_design_client[:loves_leopard]=gets.chomp
+
+puts "Do they love green(true or false)?"
+interior_design_client[:loves_green]=gets.chomp
+
