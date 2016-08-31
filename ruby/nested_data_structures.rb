edb81a81b39f05fde70664b4117e3f90aeb02269
lorelei522 <lorelei522@gmail.com>
2016-08-31 01:12:56 -0400

    Fix hash inside of an array. fix array inside of a hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,24 +1,30 @@
 movie_collection = {
-	'horror': [
-		'Friday the 13th'
-		'Candyman'
+	horror: [
+		'Friday the 13th',
+		'Candyman',
 		'Nightmare on Elm Street'
-	]
-	'romantic comedy': [
-		'10 things I Hate About You'= {
-			main_actor: 'Heath Ledger'
-			main_actress: 'Julia Stiles'
-		}
-		'When Harry met Sally'= {
-			main_actor: 'Billy Crystal'
-			main_actress: 'Meg Ryan'
-		}
-		'Annie Hall'= {
-			main_actor: 'Woody Allen'
-			main_actress: 'Diane Keaton'
-		}
-	]	
-	
+	],
+	number_1_favorite: {
+			movie_name: '10 things I Hate About You',
+			actors: [
+				'Heath Ledger',
+				'Julia Stiles',
+			]
+	},
+	number_2_favorite: {
+			movie_name: 'When Harry met Sally',
+			actors: [
+				'Billy Crystal',
+				'Meg Ryan',
+			]
+	},	
+	number_3_favorite: {
+			movie_name: 'Annie Hall',
+			actors: [
+				'Woody Allen',
+				'Diane Keaton',
+			]
+	}	
 }
 
-p movie_collection['romantic comedy'][2][main_actor]
+p movie_collection[:horror][2]
