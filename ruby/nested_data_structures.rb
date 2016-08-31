c33ffbbb5e2162778474a80636792385183808de
lorelei522 <lorelei522@gmail.com>
2016-08-31 00:40:43 -0400

    Make a hash called movie collection, add arrays for different movie genres, add movie names to that array list, add hash to each movie with about the movie

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,22 @@
+movie_collection = {
+	'Horror Genre': [
+		'Friday the 13th'
+		'Candyman'
+		'Nightmare on Elm Street'
+	]
+	'Romantic Comedy': [
+		'10 things I Hate About You'= {
+			main_actor: 'Heath Ledger'
+			main_actress: 'Julia Stiles'
+		}
+		'When Harry met Sally'= {
+			main_actor: 'Billy Crystal'
+			main_actress: 'Meg Ryan'
+		}
+		'Annie Hall'= {
+			main_actor: 'Woody Allen'
+			main_actress: 'Diane Keaton'
+		}
+	]	
+	
+}
