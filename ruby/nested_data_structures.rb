2cee736e2f39069d31fee0b1349a90d805a904ca
lorelei522 <lorelei522@gmail.com>
2016-08-31 16:16:05 -0400

    Update inside hashes to appropriate names

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,31 +2,30 @@ movie_collection = {
 	horror: [
 		'Friday the 13th',
 		'Candyman',
-		'Nightmare on Elm Street'
+		'Nightmare on Elm Street',
 	],
-	romantic: [
-		'number_1_favorite' => {
-				movie_name: '10 things I Hate About You',
-				actors: [
-					'Heath Ledger',
-					'Julia Stiles',
-				]
+	romantic: {
+		Nineties: {
+			movie_name: '10 things I Hate About You',
+			city: 'Seattle',
+			actors: ['Heath Ledger', 'Julia Stiles'],
 		},
-		'number_2_favorite' => {
-				movie_name: 'When Harry met Sally',
-				actors: [
-					'Billy Crystal',
-					'Meg Ryan',
-				]
+		Eighties: {
+			movie_name: 'When Harry met Sally',
+			city: 'New York',
+			actors: ['Billy Crystal', 'Meg Ryan'],
 		},	
-		'number_3_favorite' => {
-				movie_name: 'Annie Hall',
-				actors: [
-					'Woody Allen',
-					'Diane Keaton',
-				]
-		}
-	]
+		Seventies: {
+			movie_name: 'Annie Hall',
+			city: 'New York',
+			actors: ['Woody Allen', 'Diane Keaton'],
+		},
+		Sixities: {
+			movie_name: 'The Graduate',
+			city: 'Los Angeles',
+			actors: ['Dustin Hoffman', 'Anne Bancroft'],
+			review: '4 stars out of 5',
+		},
+	}
 }
 
-p movie_collection
