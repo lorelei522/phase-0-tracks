df3fa0f4afbd8d2dcfcb4fe99090adc92ca0612d
lorelei522 <lorelei522@gmail.com>
2016-08-31 11:56:49 -0400

    Fix the romantic array in the main hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,27 +4,29 @@ movie_collection = {
 		'Candyman',
 		'Nightmare on Elm Street'
 	],
-	number_1_favorite: {
-			movie_name: '10 things I Hate About You',
-			actors: [
-				'Heath Ledger',
-				'Julia Stiles',
-			]
-	},
-	number_2_favorite: {
-			movie_name: 'When Harry met Sally',
-			actors: [
-				'Billy Crystal',
-				'Meg Ryan',
-			]
-	},	
-	number_3_favorite: {
-			movie_name: 'Annie Hall',
-			actors: [
-				'Woody Allen',
-				'Diane Keaton',
-			]
-	}	
+	romantic: [
+		number_1_favorite: {
+				movie_name: '10 things I Hate About You',
+				actors: [
+					'Heath Ledger',
+					'Julia Stiles',
+				]
+		},
+		number_2_favorite: {
+				movie_name: 'When Harry met Sally',
+				actors: [
+					'Billy Crystal',
+					'Meg Ryan',
+				]
+		},	
+		number_3_favorite: {
+				movie_name: 'Annie Hall',
+				actors: [
+					'Woody Allen',
+					'Diane Keaton',
+				]
+		}
+	]
 }
 
 p movie_collection[:horror][2]
