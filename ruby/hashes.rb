f6bca1918e9d69a4dfac360a98a59ada4255521f
lorelei522 <lorelei522@gmail.com>
2016-08-30 22:58:29 -0400

    Make new hash with information labels that the designer needs to know about the client

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+# hash for interior design client log will include the follow on each client
+
+interior_design_client = {
+	name: "",
+	address: "",
+	age: "",
+	number_of_children: "",
+	decor_theme: "",
+	amount_of_rooms: "",
+	loves_leopard: "",
+	loves_green: "",
+}
+
