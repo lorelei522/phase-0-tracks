7c42b1bd83f52c2f481bcebb35c908300e1dce8b
lorelei522 <lorelei522@gmail.com>
2016-09-05 20:48:17 -0400

    Add comment of the answer to my search_array examples and pseudocode release 1, Fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -18,4 +18,17 @@ end
 
 array=[ 6, 7, 8, 9, 10]
 p search_array(array, 7)
+#will print 1 as the index
 p search_array(array, 15)
+#will print nil as the index since not found in the array
+
+
+#Release 1
+=begin
+Fibonacci numbers
+Start with 0 and 1 and keep adding numbers onto the array by adding
+the previous two.
+IE. Fib(6) will give you [0,1,1,2,3,5]
+Must work with Fib 100 giving you the last number of
+218922995834555169026
+end
