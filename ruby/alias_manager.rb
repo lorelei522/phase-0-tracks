a630d7378b444c0225c318561e086871c336eaa0
lorelei522 <lorelei522@gmail.com>
2016-09-05 23:46:08 -0400

    Pseudocode 5.5 Solo Challenge, add arrays for vowels and consonants

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+=begin
+5.5 Solo Challenge Pseudocode
+
+Create a method that takes any full name "John Smith" (gets.chomp)
+and turns it into a fake name were vowels go to the next(.next)
+vowel and consonsants to the next consonant. 
+
+(.split)
+
+Will be making the names into an array (.class) and join the names
+back together in the end (.join)
+
+=end
+
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
