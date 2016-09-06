acabf618c0233916a1cb73e531c8b9772a721d5a
lorelei522 <lorelei522@gmail.com>
2016-09-06 12:30:15 -0400

    add a user interface that it will loop until user saids quit on writing in original names and spitting out agent names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,14 +13,13 @@ back together in the end (.join)
 =end
 
 #determine what letter is a vowel and which is a consonant
+def alias_converter(original)
 vowel = ['a', 'e', 'i', 'o', 'u']
 consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
-puts "What is your original name? (first and last name)"
-original_name_given=gets.chomp.downcase
-
 letter= 0
 
+original_name_given= original.downcase
 #reverse the first and last name as wholes
 reverse_name=original_name_given.split(' ').reverse.join(' ')
 #split the letters of the reversed names 
@@ -44,6 +43,30 @@ split_letters_of_reversed_name.map! do |letter|
       letter
     end
 end
+new_name= split_letters_of_reversed_name.join('').split(' ').map {|i| i.capitalize}.join(' ')
+end
 
-#make sure the first letter of each word gets capitalized and join back.
-p new_name= split_letters_of_reversed_name.join('').split(' ').map {|i| i.capitalize}.join(' ')
+#Make sure the first letter of each word gets capitalized and join back.
+#Do the above block to make it happen.
+
+=begin
+Release 1: Provide a user interface that lets a user enter a name and get a 
+fake name back. Let the user do this repeatedly until they decide to quit by 
+typing 'quit'. (They might just hit Enter to continue.)
+=end
+
+inputed_name = ""
+all_names_in_session = {}
+
+until inputed_name == "quit" 
+  puts "What is your original name? (first and last name). If done write 'quit' Thank you!"
+  inputed_name = gets.chomp
+    if inputed_name != "quit"
+      all_names_in_session[inputed_name] = alias_converter(inputed_name)
+      alias_converter(inputed_name)
+    end
+end
+
+all_names_in_session.each do |original, agent_name|
+  puts "#{original} is also known as #{agent_name}"
+end  
