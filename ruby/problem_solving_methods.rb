0d85c9df7fbc7bc89a2e010f9d572ffd63b0d647
lorelei522 <lorelei522@gmail.com>
2016-09-05 20:31:19 -0400

    pseudocode release 0 of a search array and return of index

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,7 @@
+#Release 0
+=begin
+Write a method that takes an array of integers and an integer to
+search for. The method should return the index of the item or
+nil if integer not present.
+def search_array(array, number)	
+=end
