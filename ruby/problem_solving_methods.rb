78a9f4129c1249d54c815923862f883ea632811d
lorelei522 <lorelei522@gmail.com>
2016-09-05 20:33:48 -0400

    Use each to find the index since you can't use .index

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,3 +5,16 @@ search for. The method should return the index of the item or
 nil if integer not present.
 def search_array(array, number)	
 =end
+
+def  search_array (array, number)
+  start = 0
+  index = []
+  array.each do |value|
+    index << start if value == number
+    start += 1
+  end
+  index.empty? ? nil : index
+end
+
+array=[ 1, 2, 3, 4, 5]
+p search_array(array, 4)
