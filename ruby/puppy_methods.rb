b8b080ccbad990bd6f2decdb57cff499651fc572
lorelei522 <lorelei522@gmail.com>
2016-09-06 20:17:27 -0400

    Create inital Puppy class. Add methods for speak, fetch and roll over

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,18 @@
 class Puppy
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def speak(num_barks)
+		num_barks.times {puts "Woof!"}
+	end
 
-end
+	def roll_over
+		puts "*rolls over*"
+	end		
+end		
 
+jasper= Puppy.new
+jasper.speak(4)
+jasper.roll_over
