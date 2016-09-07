90e8b59fc2144abcde679cefae3f635b1a3021b7
lorelei522 <lorelei522@gmail.com>
2016-09-06 21:24:06 -0400

    Create a loop that will make 50 instrance and stores the instances in a data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -30,3 +30,34 @@ jasper.speak(4)
 jasper.roll_over
 puts jasper.dog_years(3)
 jasper.bang
+
+# Release 2- Write a new class, give it methods, initialize
+
+
+class Exercise
+	def initialize
+		puts "Person is warming up..."
+	end	
+
+	def pushups(number_pushups)
+		count= 1
+		number_pushups.times do 
+ 			puts "#{count} pushup(s)!"
+ 			count += 1
+ 		end
+ 	end
+
+ 	def pullup(number_pullups)
+ 		count= 1
+ 		number_pullups.times do
+ 			puts "#{count} pullup(s)!"
+ 			count += 1
+ 		end	
+ 	end	
+end	
+
+
+Strength_Training= []
+50.times {Strength_Training << Exercise.new}
+puts "We have #{Strength_Training.length}!"
+
