fe9ca4425cb881d9d458d5d500c45417ba74c7b4
lorelei522 <lorelei522@gmail.com>
2016-09-10 11:26:39 -0400

    add a method for yelling angrily to your Shout Module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,7 @@
 #Release 1: Write a Simple Module
 
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+  	words + "!!!" + " :("
+  end	
 end
