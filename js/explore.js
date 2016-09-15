af94e182baaba7e01b078031baf01795e4116abf
lorelei522 <lorelei522@gmail.com>
2016-09-15 07:24:25 -0400

    Pseudocode a reverse function

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Pseudocode a function that takes a string as a parameter and reverses the string
+// reverse("hello") should return "olleh"
+
+// Pseudocode:
+// you would declare the variable of the string "hello"
