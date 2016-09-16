b9f07993f7424a16302e60d4223b056de70c3512
lorelei522 <lorelei522@gmail.com>
2016-09-16 15:06:57 -0400

    Create new cars with your Car constructor function

--- js/data_structure2.js
+++ js/data_structure2.js
@@ -1,45 +1,57 @@
-var colors= ["yellow", "pink", "orange", "green"];
-var namesHorses= ["Ed", "Secretariat", "George", "Bill"];
+// create one array of four colors and one array of 4 names for your horses
+
+var colors= ['hot pink', 'sky blue', 'bright orange', 'black darkness']
+var horseNames= ['American Pharoah', 'War Admiral', 'California Chrome', 'Secretariat']
 
 
-colors.push("red");
-namesHorses.push("Alex");
+// push a new horse name and a new horse name and new color into the different arrays
 
+colors.push('fiery red')
+console.log(colors)
 
-//Release1, question 2
+horseNames.push('Man o War')
+console.log(horseNames)
 
-var horses = {}
+//Release 1-We need to assign colors to horses. In data_structures.js, add code that 
+//will use your two arrays to create an object. The keys of your object should be horse names, 
+//and the values should be colors. Your solution should be something that would work for any number of 
+//colors/horses, as long as the two arrays are the same length. This is a logical reasoning step 
+//-- try to think it through instead of Googling it.
 
-for (var i= 0; i <= colors.length; i ++) {
-	horses[namesHorses[i]] = colors [i];
+// create an empty object called horses
+var horses= {}
+//loop through the colors array. assign horses object horseNames as the key and colors as the value
+for (var i = 0; i <= colors.length - 1; i++){
+horses[horseNames[i]] = colors[i];
 }
-
+//print the new objects of horses
 console.log(horses)
-Object.keys(horses)
 
 
-//----------------
-//Release 2: constructor function for a car
+//--------------------
+//Release 2
+//write a constructor function for a car. Give it a few different properties of various
+// data types, including at least one function. Demonstrate that your function works 
+//by creating a few cars with it.
 
-function Car(model, speed, color) {
+function Car(model, year, isAutomatic) {
 	console.log("Our new car:", this);
 
 	this.model = model;
-	this.speed = speed;
-	this.color = color;
+	this.year = year;
+	this.isAutomatic= isAutomatic;
 
-	this.turnOn = function() {console.log("vroom vroom!");
-	}
+	//a function for the Car object to turnOn and print zoom zoom when it is called
+	this.turnOn = function() {console.log('zoom zoom!'); };
+
 	console.log("Car Initialization Complete");
 }
-var newcar= new Car("BMW", "120mph", "Red");
-console.log(newcar);
-newcar.turnOn();
 
-var anotherNewCar= new Car("Maserati", "150mph", "Black");
-console.log(anotherNewCar);
-anotherNewCar.turnOn();
+console.log("Let's go to the dealership and get a new car..")
 
-var ourThirdCar= new Car("Dumptruck", "65mph", "Muddy Brown");
-console.log(ourThirdCar);
-ourThirdCar.turnOn();
+var newCar= new Car ("Maserati", 2016, true);
+console.log(newCar)
+console.log("The car goes like this when it turns on..")
+//call the function we described inside of the Car constructor function
+newCar.turnOn();
+
--- js/data_structures.js
+++ js/data_structures.js
@@ -1,56 +1,47 @@
-// create one array of four colors and one array of 4 names for your horses
-
-var colors= ['hot pink', 'sky blue', 'bright orange', 'black darkness']
-var horseNames= ['American Pharoah', 'War Admiral', 'California Chrome', 'Secretariat']
+var colors= ["yellow", "pink", "orange", "green"];
+var namesHorses= ["Ed", "Secretariat", "George", "Bill"];
 
 
-// push a new horse name and a new horse name and new color into the different arrays
+colors.push("red");
+namesHorses.push("Alex");
 
-colors.push('fiery red')
-console.log(colors)
 
-horseNames.push('Man o War')
-console.log(horseNames)
+//Release1, question 2
 
-//Release 1-We need to assign colors to horses. In data_structures.js, add code that 
-//will use your two arrays to create an object. The keys of your object should be horse names, 
-//and the values should be colors. Your solution should be something that would work for any number of 
-//colors/horses, as long as the two arrays are the same length. This is a logical reasoning step 
-//-- try to think it through instead of Googling it.
+var horses = {}
 
-// create an empty object called horses
-var horses= {}
-//loop through the colors array. assign horses object horseNames as the key and colors as the value
-for (var i = 0; i <= colors.length - 1; i++){
-horses[horseNames[i]] = colors[i];
+for (var i= 0; i <= colors.length; i ++) {
+	horses[namesHorses[i]] = colors [i];
 }
-//print the new objects of horses
+
 console.log(horses)
+Object.keys(horses)
 
 
-//--------------------
-//Release 2
-//write a constructor function for a car. Give it a few different properties of various
-// data types, including at least one function. Demonstrate that your function works 
-//by creating a few cars with it.
+//----------------
+//Release 2: constructor function for a car
 
-function Car(model, year, isAutomatic) {
+function Car(model, speed, color) {
 	console.log("Our new car:", this);
 
 	this.model = model;
-	this.year = year;
-	this.isAutomatic= isAutomatic;
+	this.speed = speed;
+	this.color = color;
 
-	//a function for the Car object to turnOn and print zoom zoom when it is called
-	this.turnOn = function() {console.log('zoom zoom!'); };
-
+	this.turnOn = function() {console.log("vroom vroom!");
+	}
 	console.log("Car Initialization Complete");
 }
+var newcar= new Car("BMW", "120mph", "Red");
+console.log(newcar);
+newcar.turnOn();
 
-console.log("Let's go to the dealership and get a new car..")
+var anotherNewCar= new Car("Maserati", "150mph", "Black");
+console.log(anotherNewCar);
+anotherNewCar.turnOn();
 
-var newCar= new Car ("Maserati", 2016, true);
-console.log(newCar)
-console.log("The car goes like this when it turns on..")
-//call the function we described inside of the Car constructor function
-newCar.turnOn();
+var ourThirdCar= new Car("Dumptruck", "65mph", "Muddy Brown");
+console.log(ourThirdCar);
+ourThirdCar.turnOn();
+
+//----------------------
