dbba92eb6a44854486ab19efbbe783ca11a9c4e0
lorelei522 <lorelei522@gmail.com>
2016-09-16 12:01:11 -0400

    Add an array of colors with 4 different colors. Add an array of horses names with 4 different horse names

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+// create one array of four colors and one array of 4 names for your horses
+
+var colors= ['hot pink', 'sky blue', 'bright orange', 'black darkness']
+var horseNames= ['American Pharoah', 'War Admiral', 'California Chrome', 'Secretariat']
+
+
+// push a new horse name and a new horse name and new color into the different arrays
