c628dc869e2bedd9fcb59997740d29b839c066c8
lorelei522 <lorelei522@gmail.com>
2016-09-16 10:25:28 -0400

    Add a second way to reverse string with a loop and iteration

--- js/explore.js
+++ js/explore.js
@@ -4,8 +4,8 @@
 // Pseudocode:
 // you would declare the variable of the string "hello"
 
-
-function reverseString(str) {
+//First try with built in functions- .split, .reverse, .join
+function reverse(str) {
 //turn the string to an array that splits the string into characters
 var splitString= str.split(""); 
 
@@ -19,4 +19,28 @@ var joinArray= StringinReverseArray.join("");
 return joinArray;
 }
 
-reverseString("hello");
+console.log(reverse("hello"));
+
+
+//the above can also look like this in just one line of code
+
+// function reverse(str) {
+//	return str.split("").reverse().join("");
+// }
+// reverse("hello");
+
+
+//Second try: Reversing a string with a loop and iteration
+
+function reverse2(str) {
+	//create an empty string or an empty array that will hold the created reversed string or array
+	var updatedString = "";
+
+	// create the loop "for". The loop will start with the last letter (str.length - 1) no matter the length
+	for (var i = str.length - 1; i >= 0; i--) {
+		updatedString += str[i];
+	}
+	return updatedString;
+}
+
+console.log(reverse2("hello"));
