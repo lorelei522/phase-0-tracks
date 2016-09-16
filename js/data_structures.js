2c85792053ad23b44c5cd2c11fdcbef6df725ded
lorelei522 <lorelei522@gmail.com>
2016-09-16 12:42:00 -0400

    Add an object that takes the horses name array and the colors array and turns them into key and value for the horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,18 @@ console.log(colors)
 
 horseNames.push('Man o War')
 console.log(horseNames)
+
+//Release 1-We need to assign colors to horses. In data_structures.js, add code that 
+//will use your two arrays to create an object. The keys of your object should be horse names, 
+//and the values should be colors. Your solution should be something that would work for any number of 
+//colors/horses, as long as the two arrays are the same length. This is a logical reasoning step 
+//-- try to think it through instead of Googling it.
+
+// create an empty object called horses
+var horses= {}
+//loop through the colors array. assign horses object horseNames as the key and colors as the value
+for (var i = 0; i <= colors.length - 1; i++){
+horses[horseNames[i]] = colors[i];
+}
+//print the new objects of horses
+console.log(horses)
