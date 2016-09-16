f211900693bdba26ad296fb607ca39a5c7eea64f
lorelei522 <lorelei522@gmail.com>
2016-09-16 09:29:08 -0400

    Add a function that will reverse a string by splitting the string, reversing the split, and joining the reverse array back together

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,22 @@
 // Pseudocode a function that takes a string as a parameter and reverses the string
-// reverse("hello") should return "olleh"
+// reverse("hello") should return "olleh". There is no built in reverse method in Java
 
 // Pseudocode:
 // you would declare the variable of the string "hello"
+
+
+function reverseString(str) {
+//turn the string to an array that splits the string into characters
+var splitString= str.split(""); 
+
+//take that new split string and reverse the individual characters. Using the reverse method
+var StringinReverseArray= splitString.reverse();
+
+//now that the string is split and in reverse. Join it back together
+var joinArray= StringinReverseArray.join("");
+
+// return the reversed string and end the method
+return joinArray;
+}
+
+reverseString("hello");
