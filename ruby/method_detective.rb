4cab81eebecaf31330ac9f22d0455d2445c9d82a
lorelei522 <lorelei522@gmail.com>
2016-09-21 19:59:58 -0400

    Redid 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,47 +7,67 @@
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
+p "iNvEsTiGaTiOn".swapcase
+
 
 # "zom".<???>
 # => “zoom”
 
+p "zom".insert(2, "o")
 
 
 # "enhance".<???>
 # => "    enhance    "
 
+p "enhance".center(15)
+
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
 
+p "Stop! You’re under arrest!".upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
 
+#since -1 is the end of the string
+p "the usual".insert(-1, " suspects")
+p "the usual".concat(' suspects')
 
 # " suspects".<???>
 # => "the usual suspects"
 
+#3 ways to do it 
+#18 is for the whole length
+p " suspects".rjust(18, 'the usual')
+p " suspects".replace("the usual suspects")
+p " suspects".insert(0, "the usual")
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
 
-
+p "The case of the disappearing last letter".chop
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-
+p "The mystery of the missing first letter".reverse.chop.reverse
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-
+p "Elementary,    my   dear        Watson!".squeeze
 
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
+p "z".ord
+
+# lowercase z is 122 in the ASCII order
+
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
+
+p "How many times does the letter 'a' appear in this string?".count("a")
