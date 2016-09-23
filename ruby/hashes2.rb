3373d557dac913691e60f4ad522aefd8689776eb
lorelei522 <lorelei522@gmail.com>
2016-09-23 16:43:06 -0400

    Add all initial questions to ask the designer for info

--- ruby/hashes2.rb
+++ ruby/hashes2.rb
@@ -1,5 +1,5 @@
-# Program that allows the designer/user to input client info into a hash.
-#Have the ability for the user to update info
+# Create program that allows the designer/user to input client info into a hash.
+#Have the ability for the user to update info if needed
 
 
 Client_Info = {
@@ -16,8 +16,25 @@ Client_Info = {
 puts "What is the client name?"
 Client_Info[:client_name]= gets.chomp
 
+#make sure this data type is an integer
 puts "What is the client's age"
-Client_Info[:client_age]= gets.chomp
+Client_Info[:client_age]= gets.chomp.to_i
+
+puts "How many children does the client have?"
+Client_Info[:num_of_children]= gets.chomp.to_i
+
+puts "What is the client's decor theme?"
+Client_Info[:decor_theme]= gets.chomp
+
+puts "How many rooms does the client have in their home?"
+Client_Info[:num_of_rooms]= gets.chomp.to_i
+
+puts "Does the client like leopard? (true or false)"
+Client_Info[:likes_leopard]= gets.chomp
+
+puts "Does the client like stripes? (true or false)"
+Client_Info[:likes_stripes]= gets.chomp
+
 
 
 p Client_Info
