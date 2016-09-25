951ea1281e8ae42bda7ec9cc332f01ea97d0fb0e
lorelei522 <lorelei522@gmail.com>
2016-09-25 17:44:57 -0400

    Use .length on the array to see if the number is included and it will return the index of the number

--- ruby/problem_solving_methods2.rb
+++ ruby/problem_solving_methods2.rb
@@ -2,4 +2,21 @@
 #if the integer is in the array it will return the index(location) of the integer
 #else the integer is not in the array it will return nil
 
+array= [10, 11, 12, 13, 14, 15, 16, 17]
 
+def search_array(array, number)
+	index= nil
+	if array.include? number
+		index= 0
+		while index < array.length
+			if array[index] == number
+				return index
+			else
+			index += 1
+			end
+		end
+	end			
+end
+
+p search_array(array, 12)
+p search_array(array, 45)
