badacbf549064d83f58e79da5aef8fc18d1a9b79
lorelei522 <lorelei522@gmail.com>
2016-09-25 17:46:24 -0400

    Test out different numbers found/not found in the array

--- ruby/problem_solving_methods2.rb
+++ ruby/problem_solving_methods2.rb
@@ -1,6 +1,7 @@
 # define a method that will take an array and an integer to search for.
 #if the integer is in the array it will return the index(location) of the integer
 #else the integer is not in the array it will return nil
+# use .length to get the index
 
 array= [10, 11, 12, 13, 14, 15, 16, 17]
 
@@ -18,5 +19,7 @@ def search_array(array, number)
 	end			
 end
 
-p search_array(array, 12)
-p search_array(array, 45)
+p search_array(array, 12)   #will return 2
+p search_array(array, 45)	#will return nil
+
+
