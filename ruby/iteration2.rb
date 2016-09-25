07664e865b3a0a2e057699f1b3866365802fb167
lorelei522 <lorelei522@gmail.com>
2016-09-25 08:47:47 -0400

    Write a method that takes a block of code using yield

---
+++ ruby/iteration2.rb
@@ -0,0 +1,11 @@
+#Write own method that can take a block.
+#can use yield.
+
+
+def learning
+	puts "Hey what is the lesson of the day?"
+	lesson= "Ruby"
+	yield(lesson)
+end	
+
+learning { |lesson| puts "#{lesson} is what is being taught today." }
