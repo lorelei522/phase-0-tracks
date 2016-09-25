580c49f85abed6fa2c6134a388b3f01bd5db3724
lorelei522 <lorelei522@gmail.com>
2016-09-25 18:04:50 -0400

    Use the length of the array. Add the last numbers of the array to create a new number that will be pushed into the array

--- ruby/problem_solving_methods2.rb
+++ ruby/problem_solving_methods2.rb
@@ -23,3 +23,36 @@ p search_array(array, 12)   #will return 2
 p search_array(array, 45)	#will return nil
 
 
+#Release 1: Calculate Fibonacci Numbers
+# start with 0 and 1. keep adding numbers from the previous 2 and push in an array
+#calling the method id. fibonacci(6)---- [0,1,1,2,3,5]
+#use the index of the last two numbers to make the next number
+
+def fibonacci(number)
+	#the initial array needs 0 and 1 per instruction
+	array= [0,1]
+	#make if and elsif about if the number is 0 or 1. Any number from 2 on will have the same concept
+	if number == 0
+		p []
+	elsif number == 1
+		p [0]
+	#anything after fibonacci(1)
+	else
+		until array.length== number
+			#use the last two numbers indexs'
+			next_num= array[-2] + array[-1]
+			#push those new numbers into the array
+			array.push(next_num)
+		end
+	p array
+	end			
+end	
+
+fibonacci(0)
+fibonacci(1)
+fibonacci(2)
+fibonacci(3)
+fibonacci(4)
+fibonacci(5)
+fibonacci(6)
+fibonacci(100) #should be 218922995834555169026
