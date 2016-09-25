e8d59fc4a4ff351b235b25d21be8978d109f90bf
lorelei522 <lorelei522@gmail.com>
2016-09-25 17:36:59 -0400

    psuedocode a simple search method

---
+++ ruby/problem_solving_methods2.rb
@@ -0,0 +1,5 @@
+# define a method that will take an array and an integer to search for.
+#if the integer is in the array it will return the index(location) of the integer
+#else the integer is not in the array it will return nil
+
+
