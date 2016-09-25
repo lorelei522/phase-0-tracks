108af086d561da594148e379a1a5020d7e4d4655
lorelei522 <lorelei522@gmail.com>
2016-09-25 11:44:38 -0400

    Iterate through the colleges hash and print each college name and their location

--- ruby/iteration2.rb
+++ ruby/iteration2.rb
@@ -17,11 +17,31 @@ learning { |lesson| puts "#{lesson} is what is being taught today." }
 #array
 broadway_musicals= ["Beautiful", "Wicked", "The Lion King", "Jersey Boys", "Hamilton"]
 
+#iterate through the array with each. Shows that I changed the array within the block but
+#when the array is printed outside the block the change isn't forever.
+
+broadway_musicals.each do |musical|	
+	puts "My favorite musicals are #{musical.upcase}"
+end
+p broadway_musicals
+
+
+#make a change forever with .map!
+broadway_musicals.map! do |musical|
+	musical.swapcase
+end
+p broadway_musicals	
+
+
 #hash
 colleges= {
-	Villanova: "Villanova, PA"
-	Yale: "New Haven, CT"
-	Harvard: "Cambridge, MA"
-	UNC: "Chapel Hill, NC"
+	Villanova: "Villanova, PA",
+	Yale: "New Haven, CT",
+	Harvard: "Cambridge, MA",
+	UNC: "Chapel Hill, NC",
 	Northeastern: "Boston, MA"
 }
+
+colleges.each do |college, location|
+	puts "#{college} is located in #{location}."
+end	
