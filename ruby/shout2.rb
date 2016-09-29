9f9fd4e8adb415f0ae5bb599ba4b895e420e335c
lorelei522 <lorelei522@gmail.com>
2016-09-28 22:44:06 -0400

    Convert a standalone Module to a mixin. Add 2 classes that will include the Shout Module. Add driver code to test

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,19 +1,49 @@
 # Release 1- write a simple Module
 
 
+#module Shout
+
+#	def self.yell_angrily(words)
+#		words + "!!!" + " : ("
+#	end
+	
+#	def self.yelling_happily(words)
+#		words + " Woohoo!!!" + " YAY" + " :)"
+#	end	
+
+#end	
+
+#Driver Code for Release 1
+#p Shout.yell_angrily("You suck at your job")
+#p Shout.yelling_happily("Great work on the project.")
+
+
+#Release 3
 module Shout
 
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " : ("
 	end
 	
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + " Woohoo!!!" + " YAY" + " :)"
 	end	
 
 end	
 
+class Teacher
+	include Shout
+end
 
-#Driver Code for Release 1
-p Shout.yell_angrily("You suck at your job")
-p Shout.yelling_happily("Great work on the project.")
+class Parent
+	include Shout
+end
+
+mom= Parent.new
+p mom.yell_angrily("Clean your room")
+p mom. yelling_happily("I am so proud of you!")	
+
+ms_smith= Teacher.new
+p ms_smith.yell_angrily("Stop talking and take your seat")
+p ms_smith.yelling_happily("No homework this weekend!")
+
