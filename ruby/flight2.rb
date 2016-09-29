431b196ef1e7e745231c9fd1dd7533806dd17fd3
lorelei522 <lorelei522@gmail.com>
2016-09-28 21:28:38 -0400

    Create a Flight module

--- ruby/flight2.rb
+++ ruby/flight2.rb
@@ -1,6 +1,17 @@
 #Release 2
-
 #Create a class for a bird and a plane
+#create a Flight module above the classes for Bird and Plane
+
+module Flight
+
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude}..."
+	end
+		
+end	
+
+
+
 
 
 class Bird
@@ -9,4 +20,4 @@ end
 
 class Plane
 
-end	
+end
