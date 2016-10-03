79a21909964a71a130f38c07d13bbfc8885f16f3
lorelei522 <lorelei522@gmail.com>
2016-10-03 09:54:03 -0400

    Initialize the class with the word. Have instance variables for the progress, guesses, and letters already guessed

--- ruby/game2/game.rb
+++ ruby/game2/game.rb
@@ -3,3 +3,20 @@
 # class Game- should be initialize with a word. A method to check the letters of the word
 # guessed by player 2.
 # Track progress of the word, track number of guesses, game over and game won methods.
+# number of guesses available is related to the length of the word (.length)
+# store the letters in an empty array
+
+
+class Game
+
+	attr_reader :word
+	attr_accessor :word, :game_progress, :guess_count, :letters
+
+	def initialize(word)
+		@word = word
+		@game_progress= word.length * "__"
+		@guess_count= word.length + 3
+		@letters= []
+	end	
+
+end	
