d842ce7e9e5a54922cecf607725054ff438f8185
lorelei522 <lorelei522@gmail.com>
2016-10-03 09:45:34 -0400

    Pseudocode plan for Game class

---
+++ ruby/game2/game.rb
@@ -0,0 +1,5 @@
+#design a game that will take a word from 1 player. It gives the 2nd player a number of chances
+# to guess the word.
+# class Game- should be initialize with a word. A method to check the letters of the word
+# guessed by player 2.
+# Track progress of the word, track number of guesses, game over and game won methods.
