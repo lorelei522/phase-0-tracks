e7070d26130925dfa0773037e0350bfb3cc419aa
lorelei522 <lorelei522@gmail.com>
2016-10-10 00:51:36 -0400

    Pseudocode a function that will take an array of phrases/words and return the longest one

---
+++ js/algos2.js
@@ -0,0 +1,10 @@
+// Release 0
+// Write a function that takes an array of words and returns the longest
+// word/phrase in the array
+
+//ie. ["long phrase","longest phrase","longer phrase"]
+// function will return "longest phrase".
+
+// iterate through each of the elements in the array.
+//Compare the first element against the next for length.
+
