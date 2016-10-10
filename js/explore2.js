6cae05dc666e51677b1146d8c733018b0b9b9371
lorelei522 <lorelei522@gmail.com>
2016-10-10 00:32:19 -0400

    Debug error with the index

--- js/explore2.js
+++ js/explore2.js
@@ -8,9 +8,11 @@
 function reverse(str) {
 	var newString = "";
 
-	for (var i=str.length -1; i>= 0; i--){
-		newString += str[1];
+	for (var i = str.length - 1; i >= 0; i--){
+		newString += str[i];
 	}
 
 	return newString;
 }
+
+console.log(reverse("howdy"));
