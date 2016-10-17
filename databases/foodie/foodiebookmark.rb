5a0213986e0da64398573cd408dcb00e4e2e13b3
lorelei522 <lorelei522@gmail.com>
2016-10-17 08:01:25 -0400

    Psuedocode Food Trail app

---
+++ databases/foodie/foodiebookmark.rb
@@ -0,0 +1,7 @@
+# Foodie Trail is an app that will have two tables. 
+# 		Table one will have restaurants that you want to try
+# 		Table two will have restaurants you have tried already.
+# 		You can insert new restaurants.
+# 		Delete resturants from try list.
+# 		After you try a resturant it moves from one table to another & gets deleted from original.
+# 		3 parts to the main list- name, location, comment, triedout
